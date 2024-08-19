711eaeb00e727298ee9821adba7b9a8f916639ea
MM-Design <mauromicozzi@Host-008.homenet.telecomitalia.it>
2024-08-19 11:53:08 +0200

    Add basic code to print random messages

--- messGenerator.js
+++ messGenerator.js
@@ -1,1 +1,13 @@
-console.log('hello');
+const messagesStore = [
+    '"We cannot solve problems with the kind of thinking we employed when we came up with them". —Albert Einstein',
+    '"Learn as if you will live forever, live like you will die tomorrow.” —Mahatma Gandhi',
+    '"Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.” —Mark Twain',
+    '"When you give joy to other people, you get more joy in return. You should give a good thought to the happiness that you can give out.” —Eleanor Roosevelt'
+];
+
+const generateMessage = () => {
+    const randomNum = Math.floor(Math.random() * messagesStore.length);
+    return messagesStore[randomNum];
+}
+
+console.log(generateMessage());
